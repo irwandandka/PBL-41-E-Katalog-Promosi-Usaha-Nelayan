13d20128315c39c99066fff939519c03c65fc090
irwandandka <irwndandka@gmail.com>
2023-12-19 07:28:07 +0700

    update product detail view

--- lib/pages/product_detail.dart
+++ lib/pages/product_detail.dart
@@ -2,18 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:katalog_usaha_nelayan/theme/colors.dart';
 import '../models/Product.dart';
 import 'package:intl/intl.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 
 
 class ProductDetailPage extends StatefulWidget {
-  final Product product;
-  const ProductDetailPage({super.key, required this.product});
+  // final Product product;
+  final int productId;
+  late bool bestSeller;
+  late bool isLoad = false;
+  late int rating;
+  ProductDetailPage({super.key, required this.productId});
 
   @override
   State<ProductDetailPage> createState() => _ProductDetailPageState();
 }
 
 class _ProductDetailPageState extends State<ProductDetailPage> {
+  int get productId => widget.productId;
+  late int price;
+
+  Future<Map<String, dynamic>> fetctProduct(idProduct) async {
+    String id = idProduct.toString();
+    final response = await http
+      .get(Uri.parse("https://katalog-usaha-nelayan-ae4cb679867e.herokuapp.com/api/product/" + id));
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> dataProduct = json.decode(response.body);
+      // print(dataProduct);
+      return dataProduct;
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
+
+  late Future<Map> futureProduct;
+
+  void initState() {
+    price = 80000;
+    super.initState();
+    // futureProduct = fetctProduct(productId);
+  }
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,121 +58,175 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
         children: [
           // listview Product Detail
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: ListView(
-                children: [
-                  // Image
-                  Image.asset(
-                    widget.product.imagePath,
-                    height: 200,
-                  ),
-                  
-                  const SizedBox(height: 25),
-            
-                  // rating
-                  Visibility(
-                    visible: widget.product.isBestSeller,
-                    child: Row(
+            child: FutureBuilder(
+              future: fetctProduct(productId),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else {
+                  var product = snapshot.data;
+                  // var productModel = Product(
+                  //   name: product?['Name'] ?? '', 
+                  //   price: product?['Price'] ?? 0, 
+                  //   imagePath: product?['Image'] ?? '', 
+                  //   rating: product?['Rating'] ?? 12, 
+                  //   description: product?['Description'] ?? '', 
+                  //   seller: product?['Seller'] ?? '', 
+                  //   isBestSeller: 123,
+                  // );
+                  // return Text(product?['Name']);
+                  if(product?['isBestSeller'] == true) {
+                    widget.bestSeller = true;
+                  } else {
+                    widget.bestSeller = false;
+                  }
+                  price = product?['Price'];
+                  widget.rating = product?['Rating'];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child: ListView(
                       children: [
-                        Container(
-                          padding: EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5),
-                            color: Colors.blue[800],
-                          ),
-                          child: const Row(
+                        // Image
+                        Image.asset(
+                          product?['Image'],
+                          height: 200,
+                        ),
+                        
+                        const SizedBox(height: 25),
+                  
+                        // rating
+                        Visibility(
+                          visible: widget.bestSeller,
+                          child: Row(
                             children: [
-                              Icon(
-                                Icons.thumb_up,
-                                color: Colors.white,
-                              ),
-                              SizedBox(width: 20),
-                              // rating number
-                              Text(
-                                "Best Seller",
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold
+                              Container(
+                                padding: const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: Colors.blue[800],
                                 ),
-                                
+                                child: const Row(
+                                  children: [
+                                    Icon(
+                                      Icons.thumb_up,
+                                      color: Colors.white,
+                                    ),
+                                    SizedBox(width: 20),
+                                    // rating number
+                                    Text(
+                                      "Best Seller",
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold
+                                      ),
+                                      
+                                    ),
+                                  ],
+                                ),
                               ),
                             ],
                           ),
                         ),
-                        // Text(
-                        //   widget.product.rating,
-                        //   style: TextStyle(
-                        //     color: Colors.grey[600],
-                        //     fontWeight: FontWeight.bold 
-                        //   ),)
+                        
+                
+                        const SizedBox(height: 10),
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              product?['Name'],
+                              style: const TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  widget.rating.toString(),
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.grey[600],
+                                    fontWeight: FontWeight.bold 
+                                  ),
+                                ),
+                                Icon(Icons.star, size: 25, color: Colors.amber[300],)
+                              ],
+                            )
+                          ]
+                        ),
+                        
+                        const SizedBox(height: 25),
+
+                        Text(
+                          NumberFormat.currency(
+                            locale: 'id',
+                            symbol: 'Rp ',
+                            decimalDigits: 2,
+                          ).format(product?['Price']),
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green[400]
+                          ),
+                        ),
+
+                        const SizedBox(height: 25),
+                
+                        // Description
+                        Text(
+                          "Deskripsi",
+                          style: TextStyle(
+                            color: Colors.grey[900],
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                
+                        const SizedBox(height: 10),
+                
+                        Text(
+                          product?['Description'],
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(
+                            color: Colors.grey[800],
+                            fontSize: 15,
+                            height: 2,
+                          ),
+                        )
                       ],
                     ),
-                  ),
-                  
-
-                  const SizedBox(height: 10),
-                  
-                  // Product name
-                  Text(
-                    widget.product.name,
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-
-                  const SizedBox(height: 25),
-
-                  // Description
-                  Text(
-                    "Deskripsi",
-                    style: TextStyle(
-                      color: Colors.grey[900],
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-
-                  const SizedBox(height: 10),
-
-                  Text(
-                    widget.product.description,
-                    style: TextStyle(
-                      color: Colors.grey[800],
-                      fontSize: 15,
-                      height: 2
-                    ),
-                  )
-                ],
-              ),
+                  );
+                }
+              },
             ),
           ),
 
-          Container(
-            color: primaryColor,
-            padding: const EdgeInsets.all(25),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                      Text(
-                      NumberFormat.currency(
-                        locale: 'id',
-                        symbol: 'Rp ',
-                        decimalDigits: 2,
-                      ).format(widget.product.price),
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16
-                      ),)
-                  ],
-                )
-              ],
-            ),
-          )
+          // Container(
+          //   color: primaryColor,
+          //   padding: const EdgeInsets.all(25),
+          //   child: Column(
+          //     children: [
+          //       Row(
+          //         children: [
+          //             Text(
+          //             NumberFormat.currency(
+          //               locale: 'id',
+          //               symbol: 'Rp ',
+          //               decimalDigits: 2,
+          //             ).format(price),
+          //             style: const TextStyle(
+          //               color: Colors.white,
+          //               fontWeight: FontWeight.bold,
+          //               fontSize: 16
+          //             ),)
+          //         ],
+          //       )
+          //     ],
+          //   ),
+          // ),
         ],
       ),
     );
